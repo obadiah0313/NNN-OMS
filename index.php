c7bb56ab4708b78ee16b6f78698df73021e07b49
Obadiah <101219548@students.swinburne.edu.my>
2020-03-31 15:49:26 +0800

    create homepage

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<title>Neko Neko Nyaa</title>
+</head>
+<body>
+	<?php
+		include './NavBar.php';
+	?>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
