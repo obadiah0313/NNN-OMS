516242298452fe834883372a64b697646164941f
Obadiah <101219548@students.swinburne.edu.my>
2020-05-11 15:28:41 +0800

    Update CatalogueManage.php

--- CatalogueManage.php
+++ CatalogueManage.php
@@ -200,26 +200,6 @@
 
 					var form_data = new FormData();
 					form_data.append("file", fileUpload.files[0]);
-					var reader = new FileReader();
-					//For Browsers other than IE.
-					if (reader.readAsBinaryString) {
-						reader.onload = function(e) {
-							ProcessExcel(e.target.result);
-						};
-						reader.readAsBinaryString(fileUpload.files[0]);
-
-					} else {
-						//For IE Browser.
-						reader.onload = function(e) {
-							var data = "";
-							var bytes = new Uint8Array(e.target.result);
-							for (var i = 0; i < bytes.byteLength; i++) {
-								data += String.fromCharCode(bytes[i]);
-							}
-							ProcessExcel(data);
-						};
-						reader.readAsArrayBuffer(fileUpload.files[0]);
-					}
 					$.ajax({
 						url: './saveExcel.php',
 						method: 'POST',
@@ -230,7 +210,26 @@
 						success: function(data) {
 							data = JSON.parse(data);
 							excelfile = data.filename;
+							var reader = new FileReader();
+							//For Browsers other than IE.
+							if (reader.readAsBinaryString) {
+								reader.onload = function(e) {
+									ProcessExcel(e.target.result);
+								};
+								reader.readAsBinaryString(fileUpload.files[0]);
 
+							} else {
+								//For IE Browser.
+								reader.onload = function(e) {
+									var data = "";
+									var bytes = new Uint8Array(e.target.result);
+									for (var i = 0; i < bytes.byteLength; i++) {
+										data += String.fromCharCode(bytes[i]);
+									}
+									ProcessExcel(data);
+								};
+								reader.readAsArrayBuffer(fileUpload.files[0]);
+							}
 						}
 					});
 
@@ -301,6 +300,7 @@
 
 			$.ajax({
 				method: 'POST',
+				timeout: 5000000,
 				data: {
 					data: newObj,
 					data2: newObj2,
