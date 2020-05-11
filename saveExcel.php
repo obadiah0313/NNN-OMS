868d7c15df43e536f76663759085969daef58356
Obadiah <101219548@students.swinburne.edu.my>
2020-05-11 20:15:36 +0800

    update

--- saveExcel.php
+++ saveExcel.php
@@ -1,10 +1,12 @@
 <?php
 if(!empty($_FILES["file"]["name"]))
 {   
-	$location = '/Product_List/'.$_FILES["file"]["name"];
-	if(!file_exists($location)) {
-		chmod($location, 0755);
-		unlink($location);
+	$dir = 'Product_List/';
+	chmod($dir, 0777);
+	$path = $dir.$_FILES["file"]["name"];
+	if(!file_exists($path)) {
+		chmod($path, 0755);
+		unlink($path);
 	}
 	if(move_uploaded_file($_FILES["file"]["tmp_name"], $location))
 		echo json_encode(["filename" => $_FILES["file"]["name"]]);
