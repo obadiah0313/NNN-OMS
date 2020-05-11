56a634404ad5fff1a4e40f75cd1dc41c0918b482
Obadiah <101219548@students.swinburne.edu.my>
2020-05-11 11:41:29 +0800

    Update exportOrder.php

--- exportOrder.php
+++ exportOrder.php
@@ -1,5 +1,4 @@
 <?php
-	echo ini_get('disable_functions');
-	//exec('.\mysheet\bin\Debug\mysheet.exe 2>&1', $output);
-	//print_r($output);
+	exec('./mysheet/bin/Debug/mysheet.exe 2>&1', $output);
+	print_r($output);
 ?>
