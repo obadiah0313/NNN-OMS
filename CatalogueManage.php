7698ef6f181cae04646d6e58e530450b955ab50b
Obadiah <101219548@students.swinburne.edu.my>
2020-05-12 15:53:58 +0800

    Update CatalogueManage.php

--- CatalogueManage.php
+++ CatalogueManage.php
@@ -187,206 +187,207 @@
 </script>
 <script type="text/javascript">
 	$(document).ready(function() {
-				$('#setPrimary').hide();
-				var excelfile = "";
-				$("body").on("click", "#upload", function() {
-					//Reference the FileUpload element.
-					var fileUpload = $("#fileUpload")[0];
+		$('#setPrimary').hide();
+		var excelfile = "";
+		$("body").on("click", "#upload", function() {
+			//Reference the FileUpload element.
+			var fileUpload = $("#fileUpload")[0];
 
-					//Validate whether File is valid Excel file.
-					var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx|.xlsm)$/;
-					if (regex.test(fileUpload.value.toLowerCase())) {
-						if (typeof(FileReader) != "undefined") {
-							var form_data = new FormData();
-							form_data.append("file", fileUpload.files[0]);
-							$.ajax({
-								url: 'saveExcel.php',
-								method: 'POST',
-								data: form_data,
-								contentType: false,
-								cache: false,
-								processData: false,
-								success: function(data) {
-									data = JSON.parse(data);
-									excelfile = data.filename;
-
-									var reader = new FileReader();
-									//For Browsers other than IE.
-									if (reader.readAsBinaryString) {
-										reader.onload = function(e) {
-											ProcessExcel(e.target.result);
-										};
-										reader.readAsBinaryString(fileUpload.files[0]);
-
-									} else {
-										//For IE Browser.
-										reader.onload = function(e) {
-											var data = "";
-											var bytes = new Uint8Array(e.target.result);
-											for (var i = 0; i < bytes.byteLength; i++) {
-												data += String.fromCharCode(bytes[i]);
-											}
-											ProcessExcel(data);
-										};
-										reader.readAsArrayBuffer(fileUpload.files[0]);
-									}
-								}
-							});
-
-						} else {
-							alert("This browser does not support HTML5.");
-						}
-					} else {
-						alert("Please upload a valid Excel file.");
-					}
-
-				});
-				var product, deletion, head;
-
-				function ProcessExcel(data) {
-					//Read the Excel File data.
-					var workbook = XLSX.read(data, {
-						type: 'binary'
-					});
-
-					//Fetch the name of First Sheet.
-					var SheetList = workbook.SheetNames;
-					for (var i = 0; i < SheetList.length; i++) {
-						if (SheetList[i] == "GBD_Asia") var worksheet = SheetList[i];
-						if (SheetList[i] == "Deletions") var worksheet2 = SheetList[i];
-					}
-
-					//Read all rows from First Sheet into an JSON array.
-					var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[worksheet], {
-						raw: false
-					});
-					var newObj = [];
-					$.each(excelRows, function(index, value) {
-						if (value.hasOwnProperty("Product Code")) {
-							newObj.push(value);
-						}
-					})
-
-					var excelRows2 = XLSX.utils.sheet_to_json(workbook.Sheets[worksheet2], {
-						range: 3
-					});
-					var newObj2 = [];
-					$.each(excelRows2, function(index, value) {
-						if (value.hasOwnProperty("Code")) {
-							newObj2.push(value);
-						}
-					})
-
-
-					var headers = [];
-					var range = XLSX.utils.decode_range(workbook.Sheets[worksheet]['!ref']);
-					var C, R = range.s.r; /* start in the first row */
-					/* walk every column in the range */
-					for (C = range.s.c; C <= range.e.c; ++C) {
-						var cell = workbook.Sheets[worksheet][XLSX.utils.encode_cell({
-							c: C,
-							r: R
-						})] /* find the cell in the first row */
-
-						var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
-						if (cell && cell.t) hdr = XLSX.utils.format_cell(cell);
-
-						headers.push(hdr);
-					}
-
+			//Validate whether File is valid Excel file.
+			var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx|.xlsm)$/;
+			if (regex.test(fileUpload.value.toLowerCase())) {
+				if (typeof(FileReader) != "undefined") {
+					var form_data = new FormData();
+					form_data.append("file", fileUpload.files[0]);
 					$.ajax({
-						url: 'upload.php',
+						url: 'saveExcel.php',
 						method: 'POST',
-						data: {
-							data: newObj,
-							data3: headers,
-						},
-						success: function(response) {
-							response = JSON.parse(response);
-							for (var i = 0; i < response.header.length; i++) {
-								var li = $('<label><input type="radio" name="key" value="' + response.header[i] + '"/>' + response.header[i] + '</label><br>');
-								$('#headerlist').append(li);
-								$('#setPrimary').css("display", "block");
-								$('#setHeaders').css("display", "none");;
-							};
-							product = response.product;
-							deletion = newObj2,
-								head = headers;
-							console.log(head);
-							console.log(product);
-							console.log(deletion);
-						}
-					})
-				};
+						data: form_data,
+						contentType: false,
+						cache: false,
+						processData: false,
+						success: function(data) {
+							data = JSON.parse(data);
+							excelfile = data.filename;
 
-				function save_setting() {
-					var action = 'save_setting';
-					var cCatheader = get_setting('cCatheader');
-					var cCatfilter = get_setting('cCatfilter');
-					var pCatheader = get_setting('pCatheader');
-					var pCatfilter = get_setting('pCatfilter');
-					var cartHeader = get_setting('cartHeader');
-					$.ajax({
-						url: "./SaveSetting.php",
-						method: "POST",
-						data: {
-							action: action,
-							cCatheader: cCatheader,
-							cCatfilter: cCatfilter,
-							pCatheader: pCatheader,
-							pCatfilter: pCatfilter,
-							cartHeader: cartHeader,
-						}
-					});
-				}
+							var reader = new FileReader();
+							//For Browsers other than IE.
+							if (reader.readAsBinaryString) {
+								reader.onload = function(e) {
+									ProcessExcel(e.target.result);
+								};
+								reader.readAsBinaryString(fileUpload.files[0]);
 
-				function get_setting(class_name) {
-					var setting = [];
-					$('.' + class_name + ':checked').each(function() {
-						setting.push($(this).val());
-					});
-					return setting;
-				}
-
-				$('.common_selector').click(function() {
-					save_setting();
-				});
-
-				$('body').on("click", "#btnConfirm", function() {
-					var pk = $("input[name='key']:checked").val();
-					$.ajax({
-						method: 'POST',
-						data: {
-							data: product,
-							data3: head,
-							data4: excelfile,
-							primarykey: pk,
-						},
-						url: 'ubackend.php?doc=stock',
-						success: function(response) {
-							response = JSON.parse(response);
-							if (response.stock == "success") {
-								$.ajax({
-									method: 'POST',
-									data: {
-										data2: deletion
-									},
-									url: 'ubackend.php?doc=deletion',
-									success: function(response) {
-										response = JSON.parse(response);
+							} else {
+								//For IE Browser.
+								reader.onload = function(e) {
+									var data = "";
+									var bytes = new Uint8Array(e.target.result);
+									for (var i = 0; i < bytes.byteLength; i++) {
+										data += String.fromCharCode(bytes[i]);
 									}
-									$("body").overhang({
-										type: response.type,
-										message: response.msg,
-										callback: function() {
-											document.location.reload();
-										}
-									});
-								});
+									ProcessExcel(data);
+								};
+								reader.readAsArrayBuffer(fileUpload.files[0]);
 							}
 						}
 					});
-				});
+
+				} else {
+					alert("This browser does not support HTML5.");
+				}
+			} else {
+				alert("Please upload a valid Excel file.");
+			}
+
+		});
+		var product, deletion, head;
+
+		function ProcessExcel(data) {
+			//Read the Excel File data.
+			var workbook = XLSX.read(data, {
+				type: 'binary'
+			});
+
+			//Fetch the name of First Sheet.
+			var SheetList = workbook.SheetNames;
+			for (var i = 0; i < SheetList.length; i++) {
+				if (SheetList[i] == "GBD_Asia") var worksheet = SheetList[i];
+				if (SheetList[i] == "Deletions") var worksheet2 = SheetList[i];
+			}
+
+			//Read all rows from First Sheet into an JSON array.
+			var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[worksheet], {
+				raw: false
+			});
+			var newObj = [];
+			$.each(excelRows, function(index, value) {
+				if (value.hasOwnProperty("Product Code")) {
+					newObj.push(value);
+				}
+			})
+
+			var excelRows2 = XLSX.utils.sheet_to_json(workbook.Sheets[worksheet2], {
+				range: 3
+			});
+			var newObj2 = [];
+			$.each(excelRows2, function(index, value) {
+				if (value.hasOwnProperty("Code")) {
+					newObj2.push(value);
+				}
+			})
+
+
+			var headers = [];
+			var range = XLSX.utils.decode_range(workbook.Sheets[worksheet]['!ref']);
+			var C, R = range.s.r; /* start in the first row */
+			/* walk every column in the range */
+			for (C = range.s.c; C <= range.e.c; ++C) {
+				var cell = workbook.Sheets[worksheet][XLSX.utils.encode_cell({
+					c: C,
+					r: R
+				})] /* find the cell in the first row */
+
+				var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
+				if (cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+
+				headers.push(hdr);
+			}
+
+			$.ajax({
+				url: 'upload.php',
+				method: 'POST',
+				data: {
+					data: newObj,
+					data3: headers,
+				},
+				success: function(response) {
+					response = JSON.parse(response);
+					for (var i = 0; i < response.header.length; i++) {
+						var li = $('<label><input type="radio" name="key" value="' + response.header[i] + '"/>' + response.header[i] + '</label><br>');
+						$('#headerlist').append(li);
+						$('#setPrimary').css("display", "block");
+						$('#setHeaders').css("display", "none");;
+					};
+					product = response.product;
+					deletion = newObj2,
+						head = headers;
+					console.log(head);
+					console.log(product);
+					console.log(deletion);
+				}
+			})
+		};
+
+		function save_setting() {
+			var action = 'save_setting';
+			var cCatheader = get_setting('cCatheader');
+			var cCatfilter = get_setting('cCatfilter');
+			var pCatheader = get_setting('pCatheader');
+			var pCatfilter = get_setting('pCatfilter');
+			var cartHeader = get_setting('cartHeader');
+			$.ajax({
+				url: "./SaveSetting.php",
+				method: "POST",
+				data: {
+					action: action,
+					cCatheader: cCatheader,
+					cCatfilter: cCatfilter,
+					pCatheader: pCatheader,
+					pCatfilter: pCatfilter,
+					cartHeader: cartHeader,
+				}
+			});
+		}
+
+		function get_setting(class_name) {
+			var setting = [];
+			$('.' + class_name + ':checked').each(function() {
+				setting.push($(this).val());
+			});
+			return setting;
+		}
+
+		$('.common_selector').click(function() {
+			save_setting();
+		});
+
+		$('body').on("click", "#btnConfirm", function() {
+			var pk = $("input[name='key']:checked").val();
+			$.ajax({
+				method: 'POST',
+				data: {
+					data: product,
+					data3: head,
+					data4: excelfile,
+					primarykey: pk,
+				},
+				url: 'ubackend.php?doc=stock',
+				success: function(response) {
+					response = JSON.parse(response);
+					if (response.stock == "success") {
+						$.ajax({
+							method: 'POST',
+							data: {
+								data2: deletion
+							},
+							url: 'ubackend.php?doc=deletion',
+							success: function(response) {
+								response = JSON.parse(response);
+							}
+							$("body").overhang({
+								type: response.type,
+								message: response.msg,
+								callback: function() {
+									document.location.reload();
+								}
+							});
+						});
+					}
+				}
+			});
+		});
+	});
 
 </script>
 
