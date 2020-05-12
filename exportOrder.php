3bbc16c04ea60934496c1d1d86db346232798e8b
Obadiah <101219548@students.swinburne.edu.my>
2020-05-12 15:18:36 +0800

    Update exportOrder.php

--- exportOrder.php
+++ exportOrder.php
@@ -1,6 +1,5 @@
 <?php
- 	echo exec('ls -al -la');
 	$output = array();
-	exec('./mysheet/bin/Debug/mysheet.exe 2>&1', $output);
+	exec('/mysheet/bin/Debug/mysheet.exe 2>&1', $output);
 	var_dump($output);
 ?>
