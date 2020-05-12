fa6e92eed82f18127ba557a33c7560a976f61d60
Obadiah <101219548@students.swinburne.edu.my>
2020-05-12 15:50:52 +0800

    update upload

--- ubackend.php
+++ ubackend.php
@@ -3,32 +3,45 @@
 	//error_reporting(0);
 	require './Database.php';	
 	$db = new MongodbDatabase();
-	if (isset($_POST['data'])) {	
-		$header = [];
-		foreach($_POST['data3'] as $h){ 
-			array_push($header,$h);
-		}
-		$all = [];
-		foreach($_POST['data'] as $p){
-			$new=[];
-			foreach($p as $k=>$v){
-				$new[$k] = $v;
+	if($_GET['doc'] == 'stock')
+	{
+		if (isset($_POST['data'])) {	
+			$header = [];
+			foreach($_POST['data3'] as $h){ 
+				array_push($header,$h);
 			}
-			for($i = 0 ; $i < sizeof($header); $i++){
-				if(!array_key_exists($header[$i], $p)){
-					$new[$header[$i]] = '-';
+			$all = [];
+			foreach($_POST['data'] as $p){
+				$new=[];
+				foreach($p as $k=>$v){
+					$new[$k] = $v;
 				}
+				for($i = 0 ; $i < sizeof($header); $i++){
+					if(!array_key_exists($header[$i], $p)){
+						$new[$header[$i]] = '-';
+					}
+				}
+				array_push($all, $new);
 			}
-			array_push($all, $new);
+			$filename = $_POST['data4'];
+			$primary_key = $_POST['primarykey'];
+			if ($db->checkExists() != null) {
+				$db->replaceStock(date("Y-m-d"),$all,$header,$primary_key,$filename);
+				echo json_encode(["stock" => "success"]);
+			}
+			else{
+				$db->insertStock(date("Y-m-d"),$all,$header,$primary_key,$filename);
+				echo json_encode(["stock" => "success"]);
+			}
 		}
-		$filename = $_POST['data4'];
-		$primary_key = $_POST['primarykey'];
+	}
+	if($_GET['doc'] == 'deletion')
+	{	
 		if ($db->checkExists() != null) {
-			$db->replaceStock(date("Y-m-d"),$all,$header,$primary_key,$filename);
+			$db->replaceDeletion(date("Y-m-d"),$_POST['data2']);
 			echo json_encode(["type" => "success", "msg" => "Replaced successfully!"]);
 		}
-		else{
-			$db->insertStock(date("Y-m-d"),$all,$header,$primary_key,$filename);
+		else {
 			$db->insertDeletion(date("Y-m-d"),$_POST['data2']);
 			echo json_encode(["type" => "success", "msg" => "Insert successfully!"]);
 		}
