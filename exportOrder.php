48719af186d213f2472a2b0f9b639e6eabc8a79c
Obadiah <101219548@students.swinburne.edu.my>
2020-05-13 15:33:25 +0800

    Update exportOrder.php

--- exportOrder.php
+++ exportOrder.php
@@ -1,3 +1,8 @@
 <?php
-	exec("mysheet/bin/Debug/mysheet.exe" );
+	exec("mysheet/bin/Debug/mysheet.exe",$out,$return);
+	if (!$return) {
+    echo "Excel Created Successfully";
+	} else {
+		echo "Excel not created";
+	}
 ?>
