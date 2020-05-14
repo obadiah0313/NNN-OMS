0d9b896f0269ec8465dbd4fd08d251d0b21ddc83
Obadiah <101219548@students.swinburne.edu.my>
2020-05-15 03:23:01 +0800

    Update ubackend.php

--- ubackend.php
+++ ubackend.php
@@ -3,40 +3,30 @@
 	$db = new MongodbDatabase();
 	if($_GET['doc'] == 'stock')
 	{
-		if (isset($_POST['product']) && isset($_POST['header']) && isset($_POST['primarykey']) && isset($_POST['filename'])) {
-			/*$all = [];
-			foreach($_POST['product'] as $p){
-				$new=[];
-				foreach($p as $k=>$v){
-					$new[$k] = $v;
-				}
-				for($i = 0 ; $i < sizeof($_POST['header']); $i++){
-					if(!array_key_exists($_POST['header'][$i], $p)){
-						$new[$_POST['header'][$i]] = '-';
-					}
-				}
-				array_push($all, $new);
-			}*/
-			if ($db->checkExists() != null) {
+		if (isset($_POST['product']) && isset($_POST['header']) && isset($_POST['primarykey']) && isset($_POST['filename']))
+		{
+			var_dump($_POST['filename']);
+			/*if ($db->checkExists() != null) {
 				$db->replaceStock(date("Y-m-d"),$all,$_POST['header'],$_POST['primarykey'],$_POST['filename']);
 				echo json_encode(["stock" => "replace"]);
 			}
 			else{
 				$db->insertStock(date("Y-m-d"),$all,$_POST['header'],$_POST['primarykey'],$_POST['filename']);
 				echo json_encode(["stock" => "insert"]);
-			}
+			}*/
 		}
 	}
 	if($_GET['doc'] == 'deletion')
 	{	
-		if($_POST['process'] == "replace"){
+		var_dump($_POST['deletion']);
+		/*if($_POST['process'] == "replace"){
 			$db->replaceDeletion(date("Y-m-d"),$_POST['deletion']);
 			echo json_encode(["type" => "success", "msg" => "Replaced successfully!"]);
 		}
 		else {
 			$db->insertDeletion(date("Y-m-d"),$_POST['deletion']);
 			echo json_encode(["type" => "success", "msg" => "Insert successfully!"]);
-		}
+		}*/
 		
 	}
 ?>
