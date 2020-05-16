4da3902020dfd45feb392b45c65b1bf8a5361b9a
Obadiah <101219548@students.swinburne.edu.my>
2020-05-16 17:14:18 +0800

    use hosting db

--- checkLogin.php
+++ checkLogin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-//$manager = new MongoDB\Driver\Manager('mongodb://admin:admin123@ds239009.mlab.com:39009/heroku_0g0g5g6c?replicaSet=rs-ds239009&retryWrites=false');
+//$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$manager = new MongoDB\Driver\Manager('mongodb://admin:admin123@ds239009.mlab.com:39009/heroku_0g0g5g6c?replicaSet=rs-ds239009&retryWrites=false');
 
 $email= $_POST["email"];
 $password= md5($_POST["password"]);
@@ -11,8 +11,8 @@ $filter =['email' => $email, 'password' => $password];
 $query = new MongoDB\Driver\Query($filter);
 try {
     
-    $result = $manager->executeQuery("NNNdb.user",$query);
-    //$result = $manager->executeQuery("heroku_0g0g5g6c.user",$query);
+    //$result = $manager->executeQuery("NNNdb.user",$query);
+    $result = $manager->executeQuery("heroku_0g0g5g6c.user",$query);
     $row = $result->toArray();
     $_id =$row[0]->_id;
     $fullname = $row [0]->fullname; 
