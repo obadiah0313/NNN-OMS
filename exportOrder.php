2bf7791ba7803f0b45f2430deb8db2aead51a08f
Obadiah <101219548@students.swinburne.edu.my>
2020-05-16 17:17:34 +0800

    Update exportOrder.php

--- exportOrder.php
+++ exportOrder.php
@@ -1,10 +1,10 @@
 <?php
 	function execInBackground() {
 		if (substr(php_uname(), 0, 7) == "Windows"){
-			pclose(popen('start /B exportExcel\exportExcel.exe', 'r')); 
+			pclose(popen('start /B exportOrder\exportExcel.exe', 'r')); 
 		}
 		else {
-			exec("sudo /exporExcel/exportExcel.exe > /dev/null &");  
+			exec("/exporOrder/linux-x64/exportExcel > /dev/null &");  
 		}
 	}
 	execInBackground();
